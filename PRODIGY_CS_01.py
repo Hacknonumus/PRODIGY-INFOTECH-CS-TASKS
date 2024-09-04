020d635ac97c28efec7ce467b7c5403589a49d3b
kaulik <kaulik81@gmail.com>
2024-09-04 23:36:16 +0530

    first task 01

---
+++ PRODIGY_CS_01.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3.12
+
+import argparse
+
+def caesar_cipher(text,shift,mode):
+    result = ""
+    for char in text:
+        if char.isalpha():
+            base = ord('a') if char.islower() else ord('A')
+            if mode == 'e':
+                result_char = chr((ord(char) - base + shift) % 26 + base)
+            elif mode == 'd':
+                result_char = chr((ord(char) - base - shift) % 26 + base)
+            else:
+                raise ValueError("Invalid action. Use 'e' to encrypt or 'd' to decrypt")
+            result += result_char
+        else:
+            result += char
+    return result
+    
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="....SIMPLE SCRIPT TO ENCRYPT DECRYPT USING CAESAR CIPHER ALGHORITHEM.... ",
+    usage="""python PRODIGY_CS_01.py -m e -s 10 -t "This is part of internship at prodigy_infotech"
+    python PRODIGY_CS_01.py -m d -s 10 -t "Drsc sc zkbd yp sxdobxcrsz kd zbynsqi_sxpydomr" """)
+    
+    # input_text shift encrypt-decrypt
+    parser.add_argument("-t","--text",type=str,help="Input text for encypt or decrypt")
+    parser.add_argument("-s","--shift",type=int,help="digit to shift")
+    parser.add_argument("-m","--mode",type=str,help="mode e/d [e for encrypt/d for decrypt]")
+
+    args = parser.parse_args()
+
+    text=args.text
+    shift=args.shift
+    mode=args.mode
+    print(caesar_cipher(text,shift,mode))
+    
